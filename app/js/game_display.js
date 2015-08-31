8f5126c14ad3d6b861577ee5a3477a94dd9a5cba
Ivan <ivan@HP-Laserjet-10111.local>
2015-08-31 20:13:57 -0300

    commti

--- app/js/game_display.js
+++ app/js/game_display.js
@@ -1,33 +1,64 @@
 
-$( document ).ready(function(){
-  drawDots();
+/***************** SONGS ************************/
+
+
+global.songs = {
+  'feliz_cumple0': 'DO DO RE DO FA MI DO DO RE DO FA MI',
+  'feliz_cumple1': 'RE DO FA MI DO DO RE DO FA MI',
+  'feliz_cumple2': 'FA MI DO DO RE DO FA MI',
+  'feliz_cumple3': 'MI DO DO RE DO FA MI'
+};
+
+
+/************************************************/
+
+
+global.selectedSong = 0;
+global.currentNote  = 0;
+
+global.notes = []; //holder for the current song notes
+global.tones = ['DO','RE','MI','FA','SOL','LA','SI'];
+
+global.fruits = {
+  'DO':   'fruit_apple',
+  'RE':   'fruit_banana',
+  'MI':   'fruit_cherry',
+  'FA':   'fruit_kiwi',
+  'SOL':  'fruit_lemon',
+  'LA':   'fruit_orange',
+  'SI':   'fruit_strawberry'
+};
+
+$(document).ready(function(){
+  global.changeSong('feliz_cumple0');
 });
 
 
-function drawDots(){
-  var container = $('#dots_container');
+global.changeSong = function(song){
+  $('#fruits_container').html('');
 
-  global.column = Math.floor(($(document).height() / 124));
-  global.line = Math.floor(($(document).width() / 124));
-
-  global.totalDots = global.line * global.column;
-
-  for (var i = 0; i < global.totalDots; i++) {
-      var dot = document.createElement('div');
-      dot.id = "dot_" + i;
-      dot.className = 'dot';
-      // dot.style.borderColor = "#fff";
-      // dot.style.backgroundColor = getRandomColor();
-      // dot.style.opacity = 0.2;
-      container.append(dot);
+  global.notes = global.songs[song].split(' ');
+  for (var i = 0; i < global.notes.length; i++) {
+    var note = global.notes[i];
+    global.addFruit(global.fruits[note], i);
   }
+
+  global.currentNote = 0;
+  global.selectFruitNote(0);
 }
 
-function getRandomColor() {
-  var letters = '0123456789ABCDEF'.split('');
-  var color = '#';
-  for (var i = 0; i < 6; i++ ) {
-      color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+global.selectFruitNote = function(index){
+  $('.music-fruit').each(function(i, elem){
+    elem.className = 'music-fruit';
+    if(i == index) elem.className = 'music-fruit current-music-fruit';
+  });
+}
+
+global.addFruit = function(fruit, id){
+  var img = document.createElement('img');
+  img.id = 'fruit_' + id;
+  img.className = 'music-fruit';
+  img.src = 'img/' + fruit + '.png';
+
+  $('#fruits_container').append(img);
 }
--- app/js/touch_controller.js
+++ app/js/touch_controller.js
@@ -1,72 +1,34 @@
-AVAILABLE_NOTES = 12;
 
+var songsSelector = {
+  08: 'feliz_cumple0',
+  09: 'feliz_cumple1',
+  10: 'feliz_cumple2',
+  11: 'feliz_cumple3'
+}
 
 exports.touched = function(req, res) {
   res.send('OK');
 
-  var keyPressed = req.params.number;
+  var key = req.params.number;
 
-  if(keyPressed < 8){
-    drawRandomStar(keyPressed);
+  if(key < 8){
+    musicNotePressed(key);
   }else{
-    changeEnvAudioVariable(keyPressed);
+    global.changeSong(songsSelector[key]);
   }
-
 };
 
-var currentAudioEnv = 8;
-AUDIO_LIBS = {};
 
-// Define here the sounds path to use.
-AUDIO_LIBS['keyPressed_8']  = 'SOUND_MAPPING_0';
-AUDIO_LIBS['keyPressed_9']  = 'SOUND_MAPPING_1';
-AUDIO_LIBS['keyPressed_10'] = 'SOUND_MAPPING_2';
-AUDIO_LIBS['keyPressed_11'] = 'SOUND_MAPPING_3';
+function musicNotePressed(key){
+  var tonePressed = global.tones[key];
+  var correctTone = global.notes[global.currentNote];
 
-function changeEnvAudioVariable(num){
-  $('#keyPressed_' + currentAudioEnv).removeClass('selected');
-  $('#keyPressed_' + num).addClass('selected');
-
-  currentAudioEnv = num;
-
-  process.env['GAME_AUDIO_ENV'] = AUDIO_LIBS['keyPressed_' + currentAudioEnv];
-}
-
-
-function drawRandomStar(keyPressed){
-  var dots = getRandomPositionShapeDots();
-  for (var i = 0; i < dots.length; i++) {
-    var dot = global.window.document.getElementById('dot_' + dots[i]);
-    dot.style['-webkit-animation-name'] = 'r' + keyPressed;
-
-    dot.addEventListener('webkitAnimationEnd', function(){
-      this.style.webkitAnimationName = '';
-    }, false);
-  }
-}
-
-function getRandomPositionShapeDots(){
-  var dots = [];
-  var pivot = Math.floor(Math.random() * global.totalDots);
-
-  // Forms a diagonal square including the center point
-  dots.push(pivot);
-  dots.push(checkLimits(pivot + global.line));
-  dots.push(checkLimits(pivot + global.line - 1));
-  dots.push(checkLimits(pivot + global.line + 1));
-  dots.push(checkLimits(pivot + (global.line * 2)));
-
-  return dots;
-}
-
-function checkLimits(num){
-  if(num < 0){
-    return global.totalDots + num;
+  // TODO animations on pass and fail
+  if(tonePressed == correctTone){
+    global.currentNote++;
+  }else{
+    global.currentNote = 0;
   }
 
-  if(num >=  global.totalDots){
-    return num - global.totalDots;
-  }
-
-  return num;
+  global.selectFruitNote(global.currentNote);
 }
