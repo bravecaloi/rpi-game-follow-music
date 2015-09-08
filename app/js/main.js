6698b547c323d61a768dfbeba328e60ffb5df79d
Ivan <ivan@HP-Laserjet-10111.local>
2015-09-08 20:25:25 -0300

    remove unused libraries

--- app/js/main.js
+++
@@ -1,6 +0,0 @@
-$('.instruments a').on('click', function(){
-	$(this).find("strong").addClass('animated wobble').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
-		function(){
-			$(this).removeClass('animated wobble');
-		});
-});
